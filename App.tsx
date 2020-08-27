1f89be65ea43edf6d0f54d1187e1ff351702eb43
Phu Le <lnvtphu@gmail.com>
2020-08-27 17:58:01 +0700

    Phu Le: implement login page

--- App.tsx
+++ App.tsx
@@ -17,8 +17,12 @@ import {
   TextInput,
   View,
   Button,
-  Alert
+  Image,
+  Alert,
+  TouchableOpacity
 } from 'react-native';
+
+import rat from "./src/assests/rat.png";
 
 declare const global: {HermesInternal: null | {}};
 
@@ -27,42 +31,72 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.container}>
-        <Text>Log in</Text>
+        <Image source={rat} style={styles.logo} resizeMode="contain"/>
         <View>
-          <Text>Email</Text>
           <TextInput
             maxLength={7}
             style={styles.textInput}
             clearButtonMode="always"
+            placeholder="Username"
+            placeholderTextColor="#FFF"
           />
         </View>
         <View>
-          <Text>Password</Text>
           <TextInput
             style={styles.textInput}
             clearButtonMode="always"
             secureTextEntry={true}
+            placeholder="Password"
+            placeholderTextColor="#FFF"
+            returnKeyType="done"
           />
         </View>
-        <Button
-          onPress={() => console.log("login")}
+        {/* <Button
+          onPress={() => Alert.alert("login")}
           title="Log in"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
+          color="#00E2D6"
+        /> */}
+        <TouchableOpacity style={styles.loginBtn} onPress={() => Alert.alert("login")}>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
 };
 
+const primaryColor = "#11A2A6";
 const styles = StyleSheet.create({
   container: {
     padding: 10,
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    backgroundColor: "#232A31"
+  },
+  logo: {
+    width: "50%",
+    height: "50%",
+    alignSelf: "center",
   },
   textInput: {
-    borderColor: "#F2F1F6",
-    borderWidth: 1,
+    borderColor: primaryColor,
+    borderBottomWidth: 2,
+    height: 50,
+    color: primaryColor,
+  },
+  loginBtn: {
+    backgroundColor: primaryColor,
+    height: 50,
+    justifyContent: "center",
     borderRadius: 5,
+    marginTop: 20
+  },
+  loginText: {
+    color: "#FFF",
+    alignSelf: "center",
+    textTransform: "uppercase",
+    fontWeight: "700",
+    fontSize: 18
   }
 });
 
